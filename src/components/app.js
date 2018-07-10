6fd36bceea6396fb158efc0453a487c19fa199f4
techniX <sergei.mozhaisky@gmail.com>
2018-07-10 22:46:56 +0300

    Update ESLint rules

--- src/components/app.js
+++ src/components/app.js
@@ -10,26 +10,26 @@ import Settings from '../routes/settings';
 // import Profile from 'async!../routes/profile';
 
 export default class App extends Component {
-	
-	/** Gets fired when the route changes.
+
+  /** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	render() {
-		return (
-			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-					<Settings path="/settings" />
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="app">
+        <Header />
+        <Router onChange={this.handleRoute}>
+          <Home path="/" />
+          <Profile path="/profile/" user="me" />
+          <Profile path="/profile/:user" />
+          <Settings path="/settings" />
+        </Router>
+      </div>
+    );
+  }
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,10 +2,10 @@ import { h } from 'preact';
 import style from './style';
 
 const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
+  <div class={style.home}>
+    <h1>Home</h1>
+    <p>This is the Home component.</p>
+  </div>
 );
 
 export default Home;
