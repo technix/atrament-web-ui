a4649ba5be61f486f86899c96c71169ae52bbe4f
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2018-07-10 16:13:01 +0300

    initial commit

---
+++ _tmp/atrament-ui-vue/ui/atrament.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="wrapper">
+    <div class="atrament-scroll">
+        <div class="atrament-container" id="atrament-container">
+            <story-component :story="story"></story-component>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Story from './story.vue';
+
+export default {
+    props: ['story'],
+    components: {
+        'story-component': Story
+    }
+};
+</script>
+
+<style>
+::-webkit-scrollbar {
+    display: none;
+}
+
+.atrament-scroll {
+    width: 360px;
+    height: 600px;
+    border: 1px solid #000000;
+    overflow-y: hidden;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+@media (max-width: 400px) {
+    .atrament-scroll {
+        width: 100%;
+        height: 100vh;
+        border: none;
+    }
+}
+
+.atrament-container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+    position: relative;
+    font-family: 'Journal Sans', sans-serif;
+    line-height: 1.2em;
+}
+</style>
---
+++ _tmp/atrament-ui-vue/ui/story.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="story-container" v-if="story">
+        <scene-component v-for="scene in story" :scene="scene" :key="scene.id"></scene-component>
+    </div>
+</template>
+
+<script>
+import Scene from './scene.vue';
+
+export default {
+    props: ['story'],
+    components: {
+        'scene-component': Scene
+    }
+};
+</script>
+
+<style>
+.story-container {
+    width: 100%;
+    margin: auto;
+}
+</style>
