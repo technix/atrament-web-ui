7921bf32673a0c79304902c58aaf016efe3e4d4d
techniX <sergei.mozhaisky@gmail.com>
2018-07-14 22:14:26 +0300

    Add CSS animations for map

--- src/components/game-ui/map/index.js
+++ src/components/game-ui/map/index.js
@@ -3,21 +3,10 @@ import style from './style';
 
 const assetPath = 'assets/game/map';
 
-const currentMap = {
-  bg: 'bg-citymap.jpg',
-  points: [
-    { id: 0, x: 40, y: 420, bg: 'ae-house.png', caption: 'Дом Томаса' },
-    { id: 1, x: 60, y: 320, bg: 'ae-church.png', caption: 'Церковь' },
-    { id: 2, x: 170, y: 240, bg: 'ae-general_store.png', caption: 'Магазин' },
-    { id: 4, x: 180, y: 440, bg: 'ae-historical.png', caption: 'Гостиница' },
-    { id: 5, x: 250, y: 80, bg: 'ae-harbor.png', caption: 'Берег моря' }
-  ]
-};
-
 class Point extends Component {
   makeChoice = (e) => {
     e.preventDefault();
-    this.props.makeChoice(this.props.data.id);
+    this.props.mapChoice(this.props.data.id);
   }
   
   render({ data }) {
@@ -35,28 +24,39 @@ class Point extends Component {
 }
 
 // map component
-const Map = ({ scene, makeChoice }) => {
-  const mapPoints = scene.choices.map((c) => {
-    const m = scene.tags[c.choice];
-    if (m) {
-      return {
-        caption: m[0],
-        x: m[1],
-        y: m[2],
-        bg: m[3],
-        id: c.id
-      };
-    }
-  });
+class Map extends Component {
+  state = {
+    mapClass: [style.map, 'mapSlideIn'].join(' ')
+  }
 
-  return (
-    <div class={style.map} style={{
-      'background-image': `url(${assetPath}/${currentMap.bg})`
-    }}
-    >
-      {mapPoints.map((p) => <Point data={p} makeChoice={makeChoice} />)}
-    </div>
-  );
-};
+  mapChoice = (id) => {
+    this.setState({ mapClass: [style.map, 'mapSlideOut'].join(' ') });
+    setTimeout(() => this.props.makeChoice(id), 200);
+  }
+
+  render({ scene }, { mapClass }) {
+    const mapPoints = scene.choices.map((c) => {
+      const m = scene.tags[c.choice];
+      if (m) {
+        return {
+          caption: m[0],
+          x: m[1],
+          y: m[2],
+          bg: m[3],
+          id: c.id
+        };
+      }
+    });
+
+    return (
+      <div class={mapClass} style={{
+        'background-image': `url(${assetPath}/${scene.tags.bg})`
+      }}
+      >
+        {mapPoints.map((p) => <Point data={p} mapChoice={this.mapChoice} />)}
+      </div>
+    );
+  }
+}
 
 export default Map;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style';
+import './style/animation';
 import App from './components/app';
 
 export default App;
