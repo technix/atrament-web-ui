ab6ef1f604af3af78009f60fee2dff1423628263
techniX <sergei.mozhaisky@gmail.com>
2018-07-19 22:54:54 +0300

    Add font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import {} from 'normalize.css';
 import './style';
 import './style/animation';
+// fonts
+import './game/fonts/vollkorn/vollkorn';
+
+// app
 import App from './components/app';
 
 export default App;
