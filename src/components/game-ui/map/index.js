3d01e85646c52a12fd1da683707e5cd91c8ffd0c
techniX <sergei.mozhaisky@gmail.com>
2018-08-20 01:25:05 +0300

    Map with animations

--- src/components/game-ui/map/index.js
+++ src/components/game-ui/map/index.js
@@ -3,10 +3,14 @@ import style from './style';
 
 const assetPath = 'assets/game/map';
 
+function delay(sec) {
+  return new Promise((resolve) => setTimeout(resolve, sec));
+}
+
 class Point extends Component {
   makeChoice = (e) => {
     e.preventDefault();
-    this.props.mapChoice(this.props.data.id);
+    this.props.mapChoice(this.props.data);
   }
   
   render({ data }) {
@@ -23,37 +27,65 @@ class Point extends Component {
   }
 }
 
+const Marker = ({ data }) => (
+  <div class={style.marker} style={{
+    top: data.y,
+    left: data.x,
+    'background-image': `url(${assetPath}/player.png)`
+  }}
+  />
+);
+
+function mapPoint(m, id) {
+  console.warn(m);
+  return {
+    caption: m[0],
+    x: m[1],
+    y: m[2],
+    bg: m[3],
+    id
+  };
+}
+
 // map component
 class Map extends Component {
   state = {
-    mapClass: [style.map, 'mapSlideIn'].join(' ')
+    mapClass: [style.map, 'mapSlideIn'].join(' '),
+    mapPoints: [],
+    mapPosition: {}
   }
 
-  mapChoice = (id) => {
-    this.setState({ mapClass: [style.map, 'mapSlideOut'].join(' ') });
-    setTimeout(() => this.props.makeChoice(id), 200);
+  mapChoice = (data) => {
+    this.setState({ mapPosition: data });
+    delay(1200).then(() => {
+      this.setState({ mapClass: [style.map, 'mapSlideOut'].join(' ') });
+      setTimeout(() => this.props.makeChoice(data.id), 200);
+    });
   }
 
-  render({ scene }, { mapClass }) {
+  componentWillMount() {
+    const scene = this.props.scene;
+    // position map points
     const mapPoints = scene.choices.map((c) => {
       const m = scene.tags[c.choice];
       if (m) {
-        return {
-          caption: m[0],
-          x: m[1],
-          y: m[2],
-          bg: m[3],
-          id: c.id
-        };
+        return mapPoint(m, c.id);
       }
     });
+    // current place on map
+    const currentPosition = scene.text[0].replace(/\n|\n/g, '');
+    const mapPosition = mapPoint(scene.tags[currentPosition], 1);
+    this.setState({ mapPoints, mapPosition });
+  }
 
+  render({ scene }, { mapClass, mapPoints, mapPosition }) {
     return (
       <div class={mapClass} style={{
         'background-image': `url(${assetPath}/${scene.tags.bg})`
       }}
       >
         {mapPoints.map((p) => <Point data={p} mapChoice={this.mapChoice} />)}
+        <Marker data={mapPosition} />
       </div>
     );
   }
