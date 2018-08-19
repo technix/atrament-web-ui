def6ebfc01e8349723cbdd3afded822ca24341c3
techniX <sergei.mozhaisky@gmail.com>
2018-08-19 23:50:48 +0300

    fix config

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,9 @@
 export default function (config, env, helpers) {
   // change uglifyjs options for inkjs
-  let uglifyjs = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0].plugin;
-  uglifyjs.options.mangle = { reserved: ['Container'] };
+  let plugincfg = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+  if (plugincfg) {
+    plugincfg.plugin.options.mangle = { reserved: ['Container'] };
+  }
   // production env setup
   if (env.production) {
     config.output.publicPath = '';
