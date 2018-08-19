ad447adc790aa27fe3c0c6b8996069533af78a12
techniX <sergei.mozhaisky@gmail.com>
2018-08-19 20:48:55 +0300

    Update preact-cli config

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,8 @@
 export default function (config, env, helpers) {
+  // change uglifyjs options for inkjs
+  let uglifyjs = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0].plugin;
+  uglifyjs.options.mangle = { reserved: ['Container'] };
+  // production env setup
   if (env.production) {
     config.output.publicPath = '';
   }
