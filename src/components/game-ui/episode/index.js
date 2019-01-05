830003e77b12c9bed6a418a7eb0704757856cf62
techniX <sergei.mozhaisky@gmail.com>
2019-01-06 00:32:21 +0200

    remove demo

--- src/components/game-ui/episode/index.js
+++ src/components/game-ui/episode/index.js
@@ -3,7 +3,6 @@ import style from './style';
 
 import Choice from './choice';
 import Paragraph from './paragraph';
-import ParallaxImg from '_src_/components/blocks/parallax-img';
 
 // episode component
 class Episode extends Component {
@@ -38,50 +37,6 @@ class Episode extends Component {
           <div class={style.choiceWrapper} ref={c => this.currentChoices = c}>
             {scene.choices.map((o) => <Choice option={o} makeChoice={makeChoice} />)}
           </div>
-          <p>test 1</p>
-    <p>test 2</p>
-    <p>test 3</p>
-    <p>test 4</p>
-    <p>test 5</p>
-    <p>test 6</p>
-    <p>test 7</p>
-    <p>test 8</p>
-    <p>test 9 </p>
-    <p>test 10</p>
-    <p>test 11</p>
-    <p>test 12</p>
-    <p>test 13</p>
-    <p>test 14</p>
-    <p>test 15</p>
-    <p>test 16</p>
-    <p>test 17</p>
-    <p>test 18</p>
-    <p>test 19</p>
-    <p>test 20</p>
-
-    <ParallaxImg width={'360px'} height={'300px'} scroller={this.getScroller} />
-
-    <p>test 1</p>
-    <p>test 2</p>
-    <p>test 3</p>
-    <p>test 4</p>
-    <p>test 5</p>
-    <p>test 6</p>
-    <p>test 7</p>
-    <p>test 8</p>
-    <p>test 9 </p>
-    <p>test 10</p>
-    <p>test 11</p>
-    <p>test 12</p>
-    <p>test 13</p>
-    <p>test 14</p>
-    <p>test 15</p>
-    <p>test 16</p>
-    <p>test 17</p>
-    <p>test 18</p>
-    <p>test 19</p>
-    <p>test 20</p>
-
         </div>
       </div>
     );
