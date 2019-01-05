8cfed100cab9e38a2f0a1dea2460bb391fdb3055
techniX <sergei.mozhaisky@gmail.com>
2019-01-06 00:26:24 +0200

    Create Parallax module component

--- src/components/blocks/parallax-img/index.js
+++ src/components/blocks/parallax-img/index.js
@@ -1,32 +1,32 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 // import style from './style';
 
-class ParallaxImg extends Component {
-  scrollListener = () => {
-    const { top, bottom, height } = this.element.getBoundingClientRect();
-    const isVisible = top < this.scrollerHeight && bottom > 0;
-    this.setState({ visible: isVisible });
-    if (isVisible) {
-      let step = (this.scrollerHeight + height) / 100;
-      let scrollPercentage = Math.round(100 - ((top + height) / step));
-      this.setState({ scrollPercentage });
-    }
-  }
+import ParallaxComponent from '_src_/components/modules/parallax';
 
-  componentDidMount() {
-    this.scroller = this.props.scroller();
-    this.scrollerHeight = this.scroller.getBoundingClientRect().height;
-    this.scroller.addEventListener('scroll', this.scrollListener);
-  }
-
-  componentWillUnmount() {
-    this.scroller.removeEventListener('scroll', this.scrollListener);
-  }
-
-  render({ width, height }, { visible, scrollPercentage }) {
+class ParallaxImg extends ParallaxComponent {
+  render({ width, height }, { parallaxProgress }) {
     return (
-      <div style={`background-image: url(assets/game/gfx/postcards.jpg); background-size: cover; background-position: ${scrollPercentage}%; width: ${width}; height: ${height}`} ref={(e) => this.element = e}>
-        <div style={`width: 30px; height: 30px; position: relative; top: 30px; left: 30px; background-color: #FFFFFF; transition: 300ms linear all; transform: rotate(${scrollPercentage}deg);`} />
+      <div
+        style={`
+          background-image: url(assets/game/gfx/postcards.jpg);
+          background-size: cover;
+          background-position: ${parallaxProgress}%;
+          width: ${width};
+          height: ${height};
+        `}
+        ref={this.parallaxRef}
+      >
+        <div style={`
+          width: 30px;
+          height: 30px;
+          position: relative;
+          top: 30px;
+          left: 30px;
+          background-color: #FFFFFF;
+          transition: 300ms linear all;
+          transform: rotate(${parallaxProgress}deg);
+          `}
+        />
       </div>
     );
   }
