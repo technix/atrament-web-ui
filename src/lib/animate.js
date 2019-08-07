d9530b4d551b39d6425a36e59e50ffe693bfed51
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2019-08-07 13:20:15 +0300

    fix animateRef

--- src/lib/animate.js
+++ src/lib/animate.js
@@ -2,6 +2,7 @@ export function animateRef(ref, className) {
   return new Promise((resolve) => {
     ref.addEventListener('animationend', function animationEnd() {
       ref.removeEventListener('animationend', animationEnd);
+      ref.classList.remove(className);
       resolve();
     });
     ref.classList.remove(className);
