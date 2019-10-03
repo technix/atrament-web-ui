69feb428f3e5951b07e1eaae87f4dc13f2cb2a7c
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2019-10-03 16:47:54 +0300

    update preact

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import { Router } from 'preact-router';
 import { createHashHistory } from 'history';
 
 import { Provider, store } from '_src_/store';
+import storage from '_src_/lib/storage';
 
 import Home from './routes/home';
 import Game from './routes/game';
@@ -13,6 +14,9 @@ import Playground from './routes/playground';
 
 export default class App extends Component {
   handleRoute = e => {
+    if (e.current.props.url === '/game/new') {
+      storage.delete('auto');
+    }
     this.currentUrl = e.url;
   };
 
@@ -22,7 +26,7 @@ export default class App extends Component {
         <div id="app" ref={c => this.appContainer = c}>
           <Router onChange={this.handleRoute} history={createHashHistory()}>
             <Home path="/" />
-            <Game path="/game" />
+            <Game path="/game/:new?" />
             <Settings path="/settings" />
             <Playground path="/playground/:subcomponent?" />
           </Router>
--- src/components/routes/home/index.js
+++ src/components/routes/home/index.js
@@ -4,16 +4,12 @@ import { Link } from 'preact-router/match';
 
 import storage from '_src_/lib/storage';
 
-function deleteAutosave() {
-  storage.delete('auto');
-}
-
 const Home = () => (
   <div>
     <h1 class={style.header}>Home</h1>
     <p>This is main app screen.</p>
     {storage.exists('auto') ? <Link href="/game" class={style.button}>Continue</Link> : ''}
-    <Link href="/game" onClick={deleteAutosave} class={style.button}>{storage.exists('auto') ? 'Restart game' : 'Start game' }</Link>
+    <Link href="/game/new" class={style.button}>{storage.exists('auto') ? 'Restart game' : 'Start game' }</Link>
     <Link href="/settings" class={style.button}>Settings</Link>
     <Link href="/playground" class={style.button}><em>Playground</em></Link>
   </div>
