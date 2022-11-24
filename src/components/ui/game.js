f1702ea46e1e7758726491d684a159bf29f7ee69
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2022-11-24 11:31:19 +0200

    styling

--- src/components/ui/game.js
+++ src/components/ui/game.js
@@ -1,43 +1,11 @@
 import { h } from 'preact';
-import { useEffect, useRef } from 'preact/hooks';
-
-const UIScene = ({ scene, isOld }) => {
-  const elementRef = useRef(null);
-  const classes = ['atrament-paragraph'];
-  useEffect(() => {
-    if (!isOld) {
-      elementRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [isOld, elementRef]);
-
-  if (isOld) {
-    classes.push('old-text');
-  }  
-  return (
-    <div class={classes.join(' ')} ref={elementRef}>
-      {scene.text.map((p) => p === '' ? '' : <p>{p}</p>)}
-    </div>
-  );
-}
-
+import UIStory from './game/story';
 
 const UIGame = ({ scene, episode, makeChoice }) => {
-  function handleClick (e) {
-    const id = e.target.getAttribute('data-id');
-    makeChoice(id);
-  }
-  
-  const content = [ ...episode, scene ];
-  console.log(content);
   return (
-    <div class='atrament-storytext'>
+    <div class='atrament-game'>
       <div class='toolbar'>⚙</div>
-      <div class='atrament-storytext-content'>
-        {content.map((s, id) => <UIScene scene={s} isOld={id < content.length - 1} key={id} />)}
-        <div class='atrament-paragraph'>
-          { scene.choices.map((c) => <button class='atrament-choice' data-id={c.id} key={c.id} onClick={handleClick}>{c.choice}</button>) }
-        </div>
-      </div>
+      <UIStory scene={scene} episode={episode} makeChoice={makeChoice} />
     </div>
   );
 };
---
+++ src/components/ui/game/story.js
@@ -0,0 +1,58 @@
+import { h } from 'preact';
+import { useEffect, useRef, useCallback } from 'preact/hooks';
+
+const UIStoryScene = ({ scene, isOld }) => {
+  const elementRef = useRef(null);
+  const classes = ['atrament-paragraph'];
+  useEffect(() => {
+    if (!isOld) {
+      elementRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [isOld, elementRef]);
+
+  if (isOld) {
+    classes.push('old-text');
+  }  
+  return (
+    <div class={classes.join(' ')} ref={elementRef}>
+      {scene.text.map((p) => p === '' ? '' : <p>{p}</p>)}
+    </div>
+  );
+}
+
+const UIStoryChoices = ({ scene, handleClick }) => {
+  useEffect(() => {
+    console.log('choices rendered');
+  }, []);
+
+  return (
+    <div class='atrament-paragraph'>
+      { scene.choices.map((c) => (
+        <button class='atrament-choice' data-id={c.id} key={c.id} onClick={handleClick}>{c.choice}</button>
+      )) }
+    </div>
+  );
+};
+
+const UIStory = ({ scene, episode, makeChoice }) => {
+  const handleClick = useCallback(
+    (e) => {
+      const id = e.target.getAttribute('data-id');
+      makeChoice(id);
+    },
+    [makeChoice]
+  );
+
+  const content = [ ...episode, scene ];
+    
+  return (
+    <div class='atrament-story'>
+      <div class='atrament-story-content'>
+        {content.map((s, id) => <UIStoryScene scene={s} isOld={id < content.length - 1} key={id} />)}
+        <UIStoryChoices scene={scene} key={scene.id} handleClick={handleClick} />
+      </div>
+    </div>
+  );
+};
+  
+export default UIStory;
