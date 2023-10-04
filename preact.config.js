15c8d0214e6146c9eded597a4703915b9dcca261
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2023-10-04 20:07:37 +0300

    remove unused config

--- preact.config.js
+++ preact.config.js
@@ -3,11 +3,6 @@ import path from 'path';
 
 export default function (config, env, helpers) {
   config.resolve.modules = [ path.resolve(__dirname), 'node_modules' ];
-  // change uglifyjs options for inkjs
-  let plugincfg = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
-  if (plugincfg) {
-    plugincfg.plugin.options.mangle = { reserved: ['Container'] };
-  }
 
   // production env setup
   if (env.production) {
