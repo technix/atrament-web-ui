73d0ae21c01c17400a48696203ca52125ee6e5e6
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-05-12 17:22:29 +0300

    remove unused files from dist

--- preact.config.js
+++ preact.config.js
@@ -1,8 +1,37 @@
 import webpack from 'webpack';
 import path from 'path';
+import RemovePlugin from 'remove-files-webpack-plugin';
 
 export default function (config, env, helpers) {
   config.resolve.modules = [ path.resolve(__dirname), 'node_modules' ];
+
+  config.plugins.push(new RemovePlugin({
+    after: {
+      root: './build',
+      test: [
+        {
+          folder: '.',
+          method: (absoluteItemPath) => {
+            return new RegExp(/\.css\.map$/, 'm').test(absoluteItemPath);
+          }
+        },
+        {
+          folder: './',
+          method: (absoluteItemPath) => {
+            return new RegExp(/\.js\.map$/, 'm').test(absoluteItemPath);
+          },
+          recursive: true
+        },
+        {
+          folder: './assets/game',
+          method: (absoluteItemPath) => {
+            return new RegExp(/\.ink$/, 'm').test(absoluteItemPath);
+          },
+          recursive: true
+        }
+      ]
+    }
+  }));
 
   // production env setup
   if (env.production) {
