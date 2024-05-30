60a35a5578f2dffc677f30444669c1d10f9f35cf
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-05-30 20:25:20 +0300

    add leaflet map

--- src/components/routes/about.js
+++ src/components/routes/about.js
@@ -1,5 +1,9 @@
 import { h } from 'preact';
 import { route } from 'preact-router';
+import { useEffect } from 'preact/hooks';
+
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 import useAtrament from 'src/atrament/hooks';
 
@@ -11,29 +15,50 @@ import ContainerText from '../ui/container-text';
 import ContainerFlex from '../ui/container-flex';
 import LinkMenu from '../ui/link-menu';
 
+const initLeaflet = () => {
+  const mapMarker = L.icon({
+    iconUrl: 'assets/game/marker.png',
+    //shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [140, 140], // size of the icon
+    //shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [70, 70], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62],  // the same for the shadow
+    //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
+  const bounds = [[0,0], [1002,1417]];
+  const worldmap = L.map('worldmap', {
+    crs: L.CRS.Simple,
+    maxZoom: 2,
+    minZoom: -1,
+    zoomSnap: 0.1,
+    trackResize: true,
+    maxBounds: bounds,
+    maxBoundsViscosity: 1.0,
+    attributionControl: false
+  });
+  const mapImage = L.imageOverlay('assets/game/themap.jpg', bounds).addTo(worldmap);  
+  L.marker([ 1002 - 514, 700 ], {icon: mapMarker}).addTo(worldmap);
+  L.marker([ 1002 - 783,1030 ], {icon: mapMarker}).addTo(worldmap);
+  L.marker([ 1002 - 339, 1105 ], {icon: mapMarker}).addTo(worldmap);
+
+  worldmap.setView([500, 500], 0);
+
+};
+
+
 const AboutRoute = () => {
   const { state } = useAtrament();
   const mainMenu = () => route('/');
+  useEffect(initLeaflet, []);
 
   return (
     <Container>
       <Settings />
       <ContainerText fontSize={state.settings.fontSize}>
         <ContainerFlex>
-          <Block>
-            <img src="assets/icons/android-chrome-192x192.png" style={{margin: 'auto', display: 'block'}} />
-            <h1 style={{'text-align': 'center'}}>Atrament UI</h1>
-            <p>Web UI for Ink games, made with:</p>
-            <ul>
-              <li><a href="https://github.com/technix/atrament-core" target="_blank" rel="noreferrer">Atrament</a></li>
-              <li><a href="https://github.com/y-lohse/inkjs" target="_blank" rel="noreferrer">InkJS</a></li>
-              <li><a href="https://preactjs.com/" target="_blank" rel="noreferrer">Preact</a></li>
-              <li><a href="https://howlerjs.com/" target="_blank" rel="noreferrer">Howler</a></li>
-              <li><a href="https://github.com/nanostores/nanostores" target="_blank" rel="noreferrer">Nanostores</a></li>
-              <li><a href="https://github.com/localForage/localForage" target="_blank" rel="noreferrer">localForage</a></li>
-              <li><a href="https://github.com/101arrowz/fflate" target="_blank" rel="noreferrer">fflate</a></li>
-            </ul>
-          </Block>
+          <div id={'worldmap'} style={{width: '100%', height: '100%', 'z-index': 0}} />
         </ContainerFlex>
         <Block>
           <LinkMenu key="mainmenu" onClick={mainMenu}>Main menu</LinkMenu>
