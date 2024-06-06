ee1a9d1c4b1c55c0a78dcf35811913577f81714c
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-06-06 20:14:50 +0300

    add Ink error handler

--- src/components/routes/game.js
+++ src/components/routes/game.js
@@ -6,19 +6,25 @@ import useAtrament from 'src/atrament/hooks';
 import Container from 'src/components/ui/container';
 import Settings from 'src/components/settings';
 import Toolbar from 'src/components/ui/toolbar';
-
+import ErrorModal from 'src/components/ui/error-modal'
 import StoryView from 'src/components/views/story';
 
 const GameRoute = () => {
-  const { state, continueStory } = useAtrament();
+  const { atrament, state, continueStory } = useAtrament();
 
   useEffect(() => {
+    // register error handler
+    atrament.ink.story().onError = (error) => {
+      atrament.state.setKey('ERROR', error);
+    };
+    // continue ink story
     continueStory();
-  }, [ continueStory ]);
+  }, [ atrament, continueStory ]);
 
   return (
     <Container>
       <Settings />
+      <ErrorModal />
       <Toolbar>{state.metadata.title}</Toolbar>
       <StoryView />
     </Container>
