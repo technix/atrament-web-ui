5132a03b28f308b689c1f9f21397b08844b7a6fe
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-06-11 18:06:26 +0300

    live recompile of the ink file in dev mode

--- preact.config.js
+++ preact.config.js
@@ -1,9 +1,34 @@
 import webpack from 'webpack';
 import path from 'path';
 import RemovePlugin from 'remove-files-webpack-plugin';
+import { spawn } from 'child_process';
+
+class WatchRunPlugin {
+  constructor() {
+    this.fileTimestamps = new Map();
+  }
+
+  apply(compiler) {
+    compiler.hooks.watchRun.tap('WatchInkRecompile', (comp) => {
+      comp.fileTimestamps.forEach((v ,k) => {
+        const cached = this.fileTimestamps.get(k);
+        if (cached && cached !== v && k.endsWith('.ink')) {
+          console.log(`Recompile Ink file ${k}: ${this.fileTimestamps.get(k)} => ${v}`);
+          const inkCompilerProcess = spawn('node', ['ink-compile.js']);
+          inkCompilerProcess.stdout.on('data', (data) => console.log(data.toString()));
+          inkCompilerProcess.stderr.on('data', (data) => console.error(data.toString()));
+        }
+      });
+      this.fileTimestamps = new Map(comp.fileTimestamps);
+    });
+  }
+}
+
 
 export default function (config, env, helpers) {
   config.resolve.modules = [ path.resolve(__dirname), 'node_modules' ];
+
+  config.plugins.push(new WatchRunPlugin());
 
   config.plugins.push(new RemovePlugin({
     after: {
