10cb1355ac9ebbbb30dd53dd5c135b6ef61da3ec
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-06-18 15:14:54 +0300

    ignore incorrect tag attributes

--- src/utils/get-tag-attributes.js
+++ src/utils/get-tag-attributes.js
@@ -1,7 +1,7 @@
 export default function getTagAttributes(tag) {
   const attrs = tag.match(/(\w+)=["']?((?:.?(?!["']?\s+(?:\S+)=|\s*\/?[>"']))+.)["']?/g);
   const attributes = {};
-  attrs.forEach((item) => {
+  attrs && attrs.forEach((item) => {
     const [, name, value] = item.match(/(.+)=(.+)/);
     try {
       attributes[name] = JSON.parse(value);
