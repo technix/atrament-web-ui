3f660c7f624a334d12342198c59172c4f9d37b87
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-07-05 23:35:51 +0300

    configure singlefile build

--- vite.singlefile.config.js
+++ vite.singlefile.config.js
@@ -2,9 +2,41 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { viteSingleFile } from 'vite-plugin-singlefile';
-import { resolve } from 'path';
+import path from 'path';
+import fs from 'fs';
 
 import atramentCfg from './atrament.config.json';
+
+function findFiles(startPath, filter, callback) {
+
+  if (!fs.existsSync(startPath)) {
+    console.log("no dir ", startPath);
+    return;
+  }
+
+  const files = fs.readdirSync(startPath);
+  files.forEach((file) => {
+    const filename = path.join(startPath, file);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      findFiles(filename, filter, callback); //recurse
+    } else if (filter.test(filename)) callback(filename);
+  });
+}
+
+function removeInkFilesFromBuild() {
+  return {
+    name: 'remove-files-from-build',
+    enforce: 'post',
+    apply: 'build',
+    writeBundle(options) {
+      findFiles(options.dir, /\.ink$/, (item) => {
+        console.log(`Removing Ink file from bundle: ${item}`);
+        fs.unlinkSync(item);
+      });
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +53,7 @@ export default defineConfig({
         },
       },
     }),
+    removeInkFilesFromBuild(),
     viteSingleFile()
   ],
   resolve: {
@@ -33,14 +66,9 @@ export default defineConfig({
     port: 8900
   },
   build: {
-    outDir: 'singlefile',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.singlefile.html'),
-      },
-    },
+    outDir: 'build_singlefile'
   },
-  publicDir: false,
+  publicDir: 'root',
   base: './',
   experimental: {
     renderBuiltUrl(filename, { type, hostType }) {
