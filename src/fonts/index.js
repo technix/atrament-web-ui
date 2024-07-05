2ebaa6e0ffcd2d5097a1d603687d23f114a2a137
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-07-06 00:19:43 +0300

    add CSS safe fonts

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -6,12 +6,17 @@ import 'src/fonts/lora/index.css';
 import 'src/fonts/merriweather/index.css';
 import 'src/fonts/opendyslexic/index.css';
 
+const emojiFonts = '"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+
 export const fonts = {
-  System: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-  'Fira Sans': '"Fira Sans", sans-serif',
-  Lora: '"Lora", serif',
-  Merriweather: '"Merriweather", serif',
-  OpenDyslexic: '"OpenDyslexic", serif'
+  System: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, ${emojiFonts}`,
+  'Sans Serif': `"Trebuchet MS", "Lucida Grande", Arial, Helvetica, ${emojiFonts}`,
+  Serif: `"Palatino", "Cambria", "Lucida Bright", "Georgia", "Times New Roman", ${emojiFonts}`,
+  Monospaced: `"Monaco", "Consolas", "Lucida Console", "Courier New", ${emojiFonts}`,
+  'Fira Sans': `"Fira Sans", sans-serif, ${emojiFonts}`,
+  Lora: `"Lora", serif, ${emojiFonts}`,
+  Merriweather: `"Merriweather", serif, ${emojiFonts}`,
+  OpenDyslexic: `"OpenDyslexic", serif, ${emojiFonts}`
 };
   
 export function applyFont(font) {
