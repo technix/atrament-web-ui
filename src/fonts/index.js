3f6ad93477acb47285c40b1973710ca774eaae84
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-08-15 11:53:47 +0300

    don't include extra fonts to a singlefile build

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -1,24 +1,33 @@
 // font styles
 import { gameDefaultFont } from 'src/constants';
 
-import 'src/fonts/fira-sans/index.css';
-import 'src/fonts/lora/index.css';
-import 'src/fonts/merriweather/index.css';
-import 'src/fonts/opendyslexic/index.css';
+(async () => {
+  if (import.meta.env.MODE !== 'singlefile') {
+    await import('src/fonts/fira-sans/index.css');
+    await import('src/fonts/lora/index.css');
+    await import('src/fonts/merriweather/index.css');
+    await import('src/fonts/opendyslexic/index.css');
+  }
+})();
 
 const emojiFonts = '"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
 
-export const fonts = {
+const systemFonts = {
   System: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, ${emojiFonts}`,
   'Sans Serif': `"Trebuchet MS", "Lucida Grande", Arial, Helvetica, ${emojiFonts}`,
   Serif: `"Palatino", "Cambria", "Lucida Bright", "Georgia", "Times New Roman", ${emojiFonts}`,
-  Monospaced: `"Monaco", "Consolas", "Lucida Console", "Courier New", ${emojiFonts}`,
+  Monospaced: `"Monaco", "Consolas", "Lucida Console", "Courier New", ${emojiFonts}`
+}
+
+const extraFonts = {
   'Fira Sans': `"Fira Sans", sans-serif, ${emojiFonts}`,
   Lora: `"Lora", serif, ${emojiFonts}`,
   Merriweather: `"Merriweather", serif, ${emojiFonts}`,
   OpenDyslexic: `"OpenDyslexic", serif, ${emojiFonts}`
 };
-  
+
+export const fonts = import.meta.env.MODE === 'singlefile' ? systemFonts : { ...systemFonts, ...extraFonts };
+
 export function applyFont(font) {
   document.documentElement.style.setProperty('--font-game', fonts[font] || fonts[gameDefaultFont]);
 }
