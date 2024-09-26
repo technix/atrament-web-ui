64f6dd03f28d6f9a2d2579513c30697e3891685c
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-09-26 11:44:53 +0300

    Debugger: filtering and paginating variables list

--- src/components/ui/table/index.jsx
+++ src/components/ui/table/index.jsx
@@ -1,9 +1,43 @@
 import { h } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
 import style from './index.module.css';
 
-const Table = ({columns = [], data = []}) => {
+const Table = ({columns = [], data = [], pageSize = 0}) => {
+  const [ currentPage, setCurrentPage ] = useState(0);
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [data]);
+
+  let pages = 1;
+  let startFrom = 0;
+  let endOn = data.length;
+  if (pageSize) {
+    pages = Math.ceil(data.length/pageSize);
+    startFrom = currentPage * pageSize;
+    endOn = startFrom + pageSize;
+  }
+
+  const goToPage = (e) => {
+    setCurrentPage(+e.target.value);
+  }
+  const displayData = data.slice(startFrom, endOn);
+
   return (
     <table class={style.table}>
+      {pageSize > 0 &&
+        <caption class={style.caption}>
+          {[...Array(pages).keys()].map((p) =>
+            <button
+              onClick={goToPage}
+              value={p}
+              key={p}
+              class={p === currentPage ? style.page_active : style.page}
+            >
+              {p+1}
+            </button>
+          )}
+        </caption>
+      }
       {columns.length > 0 && 
         <thead>
           <tr>
@@ -11,9 +45,9 @@ const Table = ({columns = [], data = []}) => {
           </tr>
         </thead>
       }
-      {data.length > 0 && 
+      {displayData.length > 0 && 
         <tbody>
-          {data.map((tr, row) => (
+          {displayData.map((tr, row) => (
             <tr key={row}>
               {tr.map((td, col) => <td key={col}>{td}</td>)}
             </tr>
--- src/components/views/debugger/variables.jsx
+++ src/components/views/debugger/variables.jsx
@@ -1,4 +1,6 @@
 import { h } from 'preact';
+import style from './index.module.css';
+import { useState } from 'preact/hooks';
 import { useAtrament } from 'src/atrament/hooks';
 import { useTranslator } from '@eo-locale/preact';
 
@@ -13,21 +15,43 @@ function listInkVariables(atrament) {
   return inkVariables.sort((a, b) => a[0] > b[0] ? 0 : -1);
 }
 
-const DebugVariables = () => {
-  const { atrament } = useAtrament();
+const DebugVariablesTable = ({ variables }) => {
   const translator = useTranslator();
-  const inkVariables = listInkVariables(atrament);
-  const tableData = inkVariables.map(
+  const tableData = variables.map(
     (item, i) => [ item[0], <DebugVariableEditor key={i} name={item[0]} value={item[1]} />]
   );
   return(
+    <Table columns={
+      [
+        { name: translator.translate('debug.variables.name'), style: {width: '25%', 'text-align': 'left'} },
+        { name: translator.translate('debug.variables.value'), style: {'text-align': 'left'} }
+      ]
+    } data={tableData} pageSize={10} />
+  );
+}
+
+const DebugVariables = () => {
+  const [ varNameFilter, setVarNameFilter ] = useState('');
+  const { atrament } = useAtrament();
+  const translator = useTranslator();
+  const inkVariables = listInkVariables(atrament).filter((v) => v[0].includes(varNameFilter));
+  
+  const handleFilterChange = (e) => {
+    setVarNameFilter(e.target.value);
+  };
+
+  return(
     <Collapse title={translator.translate('debug.variables')}>
-      <Table columns={
-        [
-          { name: translator.translate('debug.variables.name'), style: {width: '25%', 'text-align': 'left'} },
-          { name: translator.translate('debug.variables.value'), style: {'text-align': 'left'} }
-        ]
-      } data={tableData} />
+      <div class={style.input_div}>
+        <input
+          class={style.input}
+          type="text"
+          placeholder={translator.translate('debug.variables.filter-by-name')}
+          value={varNameFilter}
+          onInput={handleFilterChange}
+        />
+      </div>
+      <DebugVariablesTable variables={inkVariables} />
     </Collapse>
   );
 };
