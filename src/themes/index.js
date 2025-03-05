f9ea3efe3e64a265d1c75609d70e07cd1d015922
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-03-05 22:37:03 +0200

    change theme formatting

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,47 +1,17 @@
 import { gameDefaultTheme } from 'src/constants';
-
-const lightTheme = {
-  '--bg-color': '#FCFCFC',
-  '--fg-color': '#5D576B',
-  '--shade-color': 'rgba(0, 0, 0, 0.1)',
-  '--font-color': '#333333',
-  '--accent-bg-color': '#FCFCFC',
-  '--accent-fg-color': '#F7567C',
-  '--border-radius': '0.5rem',
-  '--border-radius-inline': '0.25rem'
-};
-
-const sepiaTheme = {
-  '--bg-color': '#ffeedb',
-  '--fg-color': '#4c3b4d',
-  '--shade-color': 'rgba(0, 0, 0, 0.1)',
-  '--font-color': '#333333',
-  '--accent-bg-color': '#ffeedb',
-  '--accent-fg-color': '#a53850',
-  '--border-radius': '0.5rem',
-  '--border-radius-inline': '0.25rem'
-}
-
-const darkTheme = {
-  '--bg-color': '#4c3b4d',
-  '--fg-color': '#FCFCFC',
-  '--shade-color': 'rgba(255, 255, 255, 0.1)',
-  '--font-color': '#EEEEEE',
-  '--accent-bg-color': '#EEEEEE',
-  '--accent-fg-color': '#F7567C',
-  '--border-radius': '0.5rem',
-  '--border-radius-inline': '0.25rem'
-};
+import lightTheme from './light.json';
+import sepiaTheme from './sepia.json';
+import darkTheme from './dark.json';
 
 export const themes = {
-  light: lightTheme,
-  sepia: sepiaTheme,
-  dark: darkTheme
+  light: lightTheme.theme,
+  sepia: sepiaTheme.theme,
+  dark: darkTheme.theme
 };
 
 export function applyTheme(theme) {
   Object.entries(themes[theme] || themes[gameDefaultTheme]).forEach(([prop, value]) => {
-    document.documentElement.style.setProperty(prop, value);
+    document.documentElement.style.setProperty(`--${prop}`, value);
   });
 }
 
