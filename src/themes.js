4665cea66bb3636cae34d2842ee56f983d9d1ce0
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-03-06 14:02:42 +0200

    dynamic add/remove themes

--- src/themes.js
+++ src/themes.js
@@ -1,13 +1,26 @@
 import { gameDefaultTheme } from 'src/constants';
-import lightTheme from '../resources/themes/light.json';
-import sepiaTheme from '../resources/themes/sepia.json';
-import darkTheme from '../resources/themes/dark.json';
 
-export const themes = {
-  light: lightTheme.theme,
-  sepia: sepiaTheme.theme,
-  dark: darkTheme.theme
-};
+// import theme modules
+const themes = {};
+
+const allThemes = {};
+const modules = import.meta.glob('../resources/themes/*.json', { eager: true });
+Object.values(modules).map(({ default: theme }) => {
+  if (theme) {
+    allThemes[theme.name] = theme.theme;
+  }
+});
+// add internal themes, if they are present
+['light', 'sepia', 'dark'].forEach((t) => {
+  if (allThemes[t]) {
+    themes[t] = allThemes[t];
+    delete allThemes[t];
+  }
+});
+// add the rest of the themes
+Object.keys(allThemes).sort().forEach(t => (themes[t] = allThemes[t]));
+
+export { themes };
 
 export function applyTheme(theme) {
   Object.entries(themes[theme] || themes[gameDefaultTheme]).forEach(([prop, value]) => {
