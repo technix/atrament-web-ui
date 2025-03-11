f4d9d734e31ecd2e6419a6eac7ba2e9308762dc6
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-03-11 15:23:38 +0200

    atrament 2.1.0

---
+++ assets/index-B5Af6un6.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./index-GjKFdm-Z.css"])))=>i.map(i=>d[i]);
+import{_ as a}from"./index-B5RvxMBj.js";a(()=>Promise.resolve({}),__vite__mapDeps([0]),import.meta.url);const e={name:"Fira Sans",fallback:"sans-serif"};export{e as default};
