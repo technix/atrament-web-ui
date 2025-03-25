d391a3d110634af084950d1ed7964097ba020fb0
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-03-25 16:08:00 +0200

    Neutralino build improvements

--- tools/prepare-neutralino.cjs
+++ tools/prepare-neutralino.cjs
@@ -1,5 +1,9 @@
+const { spawnSync } = require('node:child_process');
 const fs = require('node:fs');
+const copy = require('recursive-copy');
 const cfg = require('../atrament.config.json');
+
+const TOOLS_DIR = 'tools/neutralino';
 
 const neutralinoConfig = {
   $schema: "https://raw.githubusercontent.com/neutralinojs/neutralinojs/main/schemas/neutralino.config.schema.json",
@@ -52,7 +56,37 @@ const neutralinoConfig = {
   }
 };
 
+
+
+// 1. Create config file
+
+if (!fs.existsSync(TOOLS_DIR)) {
+  fs.mkdirSync(TOOLS_DIR);
+}
 fs.writeFileSync(
-  "build/.tmp_neutralino/neutralino.config.json",
+  `${TOOLS_DIR}/neutralino.config.json`,
   JSON.stringify(neutralinoConfig, undefined, "  ")
 );
+
+// 2. Download Neutralino binaries, if needed
+
+if (!fs.existsSync(`${TOOLS_DIR}/bin`)) {
+  console.info(`Downloading Neutralino binaries...`);
+  const res = spawnSync('npm', ['run', 'install-neutralino']);
+  [ res.stdout.toString(), res.stderr.toString() ].forEach(
+    (item) => item && console.log(item)
+  );
+}
+if (!fs.existsSync(`${TOOLS_DIR}/bin`)) {
+  console.error("Failed to install Neutralino");
+  process.exit(1);
+}
+
+// 3. Copy Neutralino to builddir
+
+copy(TOOLS_DIR, "build/.tmp_neutralino", { overwrite: true })
+  .then((results) => {
+    console.info(`Copied ${results.length} files`);
+  }).catch((error) => {
+    console.error(`Copy failed: ${error}`);
+  });
