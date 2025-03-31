ec6ceff62558564ae150904c15336e1d7ec4fff1
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-03-31 15:09:47 +0300

    feature: timed click to continue

--- src/components/views/story/click-to-continue/index.jsx
+++ src/components/views/story/click-to-continue/index.jsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from 'preact/hooks';
 import { useTranslator } from '@eo-locale/preact';
 import { useAtrament } from 'src/atrament/hooks';
 
-const ClickToContinue = ({ setReady, withChoice = false }) => {
+const ClickToContinue = ({ setReady, withChoice = false, timeout = 0 }) => {
   const { makeChoice, continueStory } = useAtrament();
   const [ isVisible, setIsVisible ] = useState(false);
   const translator = useTranslator();
@@ -26,22 +26,33 @@ const ClickToContinue = ({ setReady, withChoice = false }) => {
   }, [ continueGame ]);
 
   useEffect(() => {
+    let timedChoice;
     document.addEventListener("keydown", kbdChoiceHandler, false);
     document.getElementsByClassName('atrament-container-scene')[0].addEventListener("click", continueGame, false);
     document.getElementsByClassName('atrament-container-choices')[0].addEventListener("click", continueGame, false);
-    setTimeout(() => {
-      setIsVisible(true);
-    }, 3000);
+    if (timeout) {
+      timedChoice = setTimeout(() => {
+        continueGame();
+      }, timeout * 1000);
+    } else {
+      setTimeout(() => {
+        setIsVisible(true);
+      }, 3000);
+    }
     return () => {
+      if (timedChoice) {
+        clearTimeout(timedChoice);
+      }
       document.removeEventListener("keydown", kbdChoiceHandler, false);
       document.getElementsByClassName('atrament-container-scene')[0].removeEventListener("click", continueGame, false);
       document.getElementsByClassName('atrament-container-choices')[0].removeEventListener("click", continueGame, false);
     }
-  }, [ kbdChoiceHandler, continueGame, setIsVisible ]);
+  }, [ kbdChoiceHandler, continueGame, setIsVisible, timeout ]);
 
   return (
     <div class={style.container}>
       {isVisible && <div class={`${style.circle}`} title={translator.translate('game.click-to-continue')} />}
+      {timeout && <div class={[style.circle, style.circle_empty].join(' ')} title={translator.translate('game.click-to-continue')} />}
     </div>
   ) 
 };
