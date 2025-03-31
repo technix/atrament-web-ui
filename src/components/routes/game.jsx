053ec6b0c5ad034342521a059f062907af953cbc
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-03-31 12:46:18 +0300

    reorganize source

--- src/components/routes/game.jsx
+++ src/components/routes/game.jsx
@@ -5,11 +5,12 @@ import { useAtrament, useAtramentState } from 'src/atrament/hooks';
 
 import Container from 'src/components/ui/container';
 import Menu from 'src/components/menu';
-import StoryError from 'src/components/ui/error-story';
+
 
 import Toolbar from 'src/components/views/toolbar';
 import StoryView from 'src/components/views/story';
 import OverlayView from 'src/components/views/overlay';
+import StoryError from 'src/components/views/story-error';
 import { setPageBackground } from 'src/utils/page-background';
 
 const GameRoute = () => {
