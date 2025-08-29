814a8aca563b13e9acb12cae6ab3a6903d3352b8
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-08-29 13:13:39 +0300

    improved preloader

--- src/components/views/overlay/index.jsx
+++ src/components/views/overlay/index.jsx
@@ -1,21 +1,42 @@
 import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 
-import { useAtramentOverlay } from 'src/atrament/hooks';
+import { useAtrament, useAtramentOverlay } from 'src/atrament/hooks';
+import preloadImages from 'src/utils/preload-images';
+import getImagesFromContent from "src/utils/get-images-from-content";
+
 import TextParagraph from 'src/components/ui/text-paragraph';
 import Markup from 'src/components/ui/markup';
+import CircleLoader from 'src/components/ui/animation-circles';
 
 import OverlayPresenter from './overlay';
 import ModalPresenter from './modal';
 
 
 const OverlayView = () => {
+  const { getAssetPath } = useAtrament();
   const { overlay, closeOverlay } = useAtramentOverlay();
+  const [ isLoaded, setIsLoaded ] = useState(false);
+
+  const preloader = async () => {
+    await preloadImages(getAssetPath, getImagesFromContent(overlay.content));
+    setIsLoaded(true);
+  }
+
+  // preload all images for overlay
+  useEffect(() => {
+    if (overlay.current) {
+      preloader();
+    }
+  });
 
   if (!overlay.current) {
     return <></>;
   }
 
-  const content = overlay.content.map((item, index) => <TextParagraph key={index}><Markup content={item} /></TextParagraph>);
+  const content = isLoaded
+    ? overlay.content.map((item, index) => <TextParagraph key={index}><Markup content={item} /></TextParagraph>)
+    : <CircleLoader />;
 
   if (overlay.display === 'modal') {
     return (
