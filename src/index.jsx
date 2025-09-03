71e0cc49b44d7019e0f82dd69f187537ca800814
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-09-03 12:38:06 +0300

    fix: custom styles should override existing ones

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import { render } from 'preact';
 // CSS
 import 'normalize.css';
 import 'src/app.css';
-import '../resources/styles/custom.css';
 
 // App
 import App from './components/app';
 
 // Patches
 import 'src/utils/neutralino-out-of-bounds-fix';
+
+// Override CSS styles
+import '../resources/styles/custom.css';
 
 render(<App />, document.getElementById('app'));
