a7640d6c54b52e606de5846425ef81449df9c12a
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-09-11 13:10:48 +0300

    fix: use first = sign for attribute value parsing

--- src/utils/get-tag-attributes.js
+++ src/utils/get-tag-attributes.js
@@ -2,7 +2,7 @@ export default function getTagAttributes(tag) {
   const attrs = tag.match(/(\w+)=["']?((?:.?(?!["']?\s+(?:\S+)=|\s*\/?[>"']))+.)["']?/g);
   const attributes = {};
   attrs && attrs.forEach((item) => {
-    const [, name, value] = item.match(/(.+)=(.+)/);
+    const [, name, value] = item.match(/(.+?)=(.+)/);
     try {
       attributes[name] = JSON.parse(value);
     } catch(e) {
