73a20e693e81863ab64160db208b9a2f5a581b66
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-12-28 20:17:53 +0200

    move progressbar to UI components

--- src/components/markup/progress/index.jsx
+++ src/components/markup/progress/index.jsx
@@ -1,36 +1,10 @@
 import { h } from 'preact';
 import clsx from 'clsx';
-import style from './index.module.css';
+import ProgressBar from 'src/components/ui/progressbar';
 
 // [progress min=0 max=100 value=99 style=accent]text in bar[/progress]
 
-const Progress = ({ options, children }) => {
-  const min = +options.min || 0;
-  const max = +options.max || 100;
-  const value = +options.value || 0;
-  let width = value ? ((value - min) * 100)/(max - min) : 0;
-  if (width > 100) {
-    width = 100;
-  }
-  if (width < 0) {
-    width = 0;
-  }
-
-  const classList = clsx(
-    style.progress_bar,
-    options.style === 'accent' ? style.accent : style.standard,
-    'atrament-tag-progressbar-gauge'
-  );
-
-  return (
-    <div class={clsx(style.progress_frame, 'atrament-tag-progressbar', options.class)}>
-      <div class={classList} style={{ width:`${width}%` }} />
-      <div class={clsx(style.progress_content, 'atrament-tag-progressbar-content')}>{children}&nbsp;</div>
-    </div>
-  );
-};
-
 export default {
   tag: 'progress',
-  component: Progress
+  component: ProgressBar
 }
---
+++ src/components/ui/progressbar/index.jsx
@@ -0,0 +1,40 @@
+import { h } from 'preact';
+import clsx from 'clsx';
+import style from './index.module.css';
+
+const ProgressBar = ({ options, children }) => {
+  const min = +options.min || 0;
+  const max = +options.max || 100;
+  const value = +options.value || 0;
+  let width = value ? ((value - min) * 100)/(max - min) : 0;
+  if (width > 100) {
+    width = 100;
+  }
+  if (width < 0) {
+    width = 0;
+  }
+
+  const classListFrame = clsx(
+    style.progress_frame,
+    options.border === false && style.borderless,
+    'atrament-tag-progressbar',
+    options.class
+  );
+
+  const classListGauge = clsx(
+    style.progress_bar,
+    options.style === 'accent' ? style.accent : style.standard,
+    'atrament-tag-progressbar-gauge'
+  );
+
+  return (
+    <div class={classListFrame}>
+      <div class={classListGauge} style={{ width:`${width}%` }} />
+      <div class={clsx(style.progress_content, 'atrament-tag-progressbar-content')}>
+        {children}{options.display === 'thin' ? '' : <>&nbsp;</>}
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
