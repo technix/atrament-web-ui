21fd389b1538d5eea8ea90d4544d3c7a92a2261c
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-01-08 13:07:39 +0200

    fix: the backdrop does not cover the whole app area

--- src/utils/set-inner-height.js
+++ src/utils/set-inner-height.js
@@ -3,7 +3,7 @@ let vh;
 function setInnerHeight () {
   if (window.innerHeight !== vh) {
     vh = window.innerHeight;
-    document.documentElement.style.setProperty('--screen-vh', `${vh - 1}px`);
+    document.documentElement.style.setProperty('--screen-vh', `${vh}px`);
   }
 }
 
