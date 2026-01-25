5fcc726a798f55665a8c166ca66fab530c0d847b
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-01-25 17:57:07 +0200

    feat: css class for read paragraph

--- src/components/ui/text-paragraph/index.jsx
+++ src/components/ui/text-paragraph/index.jsx
@@ -3,7 +3,13 @@ import clsx from 'clsx';
 import style from './index.module.css';
 
 const TextParagraph = ({ children, active = true, class_override = '' }) => (
-  <div class={clsx(style.paragraph, !active && style.inactive, 'atrament-paragraph', class_override)}>
+  <div class={clsx(
+    style.paragraph,
+    !active && style.inactive,
+    'atrament-paragraph',
+    !active && 'atrament-paragraph-read',
+    class_override
+  )}>
     {children}
   </div>
 );
