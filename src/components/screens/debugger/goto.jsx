e0ef3941e26eedb2b4937a55b655a64b86db9671
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-01-27 18:26:15 +0200

    feature: debugger "goTo" now has list of all available knots

--- src/components/screens/debugger/goto.jsx
+++ src/components/screens/debugger/goto.jsx
@@ -1,18 +1,40 @@
 import { h } from 'preact';
 import style from './index.module.css';
-import { Text } from '@eo-locale/preact';
-import { useState } from 'preact/hooks';
+import { useTranslator } from '@eo-locale/preact';
+import { useEffect, useState } from 'preact/hooks';
 import { useAtrament } from 'src/atrament/hooks';
+import Collapse from 'src/components/ui/collapse';
+
+const getPathList = (atrament) => {
+  const inkKnots = [];
+  const contentRoot = atrament.ink.story().mainContentContainer.namedContent;
+  for (const key of contentRoot.keys()) {
+    if (key !== 'global decl') {
+      // this is a knot
+      inkKnots.push(key);
+    }
+    for (const subkey of contentRoot.get(key).namedContent.keys()){
+      inkKnots.push(`${key}.${subkey}`);
+    }
+  }
+  return inkKnots.sort();
+}
+
 
 const DebugGoto = ({ closeFn }) => {
   const { atrament } = useAtrament();
+  const translator = useTranslator();
+  const [ inkPathList, setInkPathList ] = useState([]);
+  const [ pathNameFilter, setPathNameFilter ] = useState('');
   const [ pathString, setPathString ] = useState('');
   const [ errorMsg, setErrorMsg ] = useState('');
 
   const handlePathstringChange = (e) => {
     setPathString(e.target.value);
+    setPathNameFilter(e.target.value);
     setErrorMsg('');
   };
+
   const goToPath = () => {
     if (pathString) {
       try {
@@ -23,27 +45,44 @@ const DebugGoto = ({ closeFn }) => {
         setErrorMsg(e.toString());
       }
       setPathString('');
+      setPathNameFilter('');
     }
   };
 
+  const setPath = (ev) => {
+    const chosenPath = ev.target.getAttribute('data-path');
+    setPathString(chosenPath);
+    setPathNameFilter(chosenPath);
+  }
+
+  useEffect(() => {
+    setInkPathList(getPathList(atrament));
+  }, [ atrament ]);
+
+  const inkPaths = inkPathList.filter((v) => v.includes(pathNameFilter));
+
   return (
     <>
-      <div class={style.container}>
-        <div>
-          <Text id={'debug.go-to-path'} />:
+      <Collapse title={`${translator.translate('debug.go-to-path')}`}>
+        <div class={style.container}>
+          <div class={style.input_div}>
+            <input class={style.input} type="text" value={pathString} onInput={handlePathstringChange} />
+          </div>
+          <div>
+            <button class={style.button} onClick={goToPath}>&gt;&gt;&gt;</button> <br />
+          </div>
         </div>
-        <div class={style.input_div}>
-          <input class={style.input} type="text" value={pathString} onInput={handlePathstringChange} />
+        <div class={style.container_scrollable}>
+          <ul>
+            {inkPaths.map((p) => <li><a href="#" onClick={setPath} data-path={p}>{p}</a></li>)}
+          </ul>
         </div>
-        <div>
-          <button class={style.button} onClick={goToPath}>&gt;&gt;&gt;</button> <br />
+        <div class={style.container}>
+          <span class={style.errormsg}>{errorMsg}</span>
         </div>
-      </div>
-      <div class={style.container}>
-        <span class={style.errormsg}>{errorMsg}</span>
-      </div>
+      </Collapse>
     </>
   );
-};
+}
 
 export default DebugGoto;
