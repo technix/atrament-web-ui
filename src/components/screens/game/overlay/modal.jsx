a47ef4168a08f81c5550309cbf2ec9ca7705a6fd
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-01-29 12:25:32 +0200

    feature: CSS classes for overlays

--- src/components/screens/game/overlay/modal.jsx
+++ src/components/screens/game/overlay/modal.jsx
@@ -9,16 +9,32 @@ import Modal from 'src/components/ui/modal';
 import CloseButton from 'src/components/ui/close-button';
 
 export default function ModalPresenter({ children, title, closeOverlay }) {
+  const headerClasses = clsx(
+    style.overlay_header,
+    style.overlay_header_modal,
+    title && style.overlay_header_bottom_line,
+    'atrament-overlay-header-modal',
+  );
+  const titleClasses = clsx(
+    style.overlay_title,
+    style.overlay_title_modal,
+    'atrament-overlay-title-modal',
+  );
+  const overlayClasses = clsx(
+    style.overlay_content,
+    'atrament-overlay-content-modal'
+  );
+
   return (
     <ContainerModal>
       <Backdrop onClick={closeOverlay} />
       <Modal>
-        <div class={clsx(style.overlay_header, style.overlay_header_modal, title && style.overlay_header_bottom_line)}>
-          <div class={clsx(style.overlay_title, style.overlay_title_modal)}>{title}</div>
+        <div class={headerClasses}>
+          <div class={titleClasses}>{title}</div>
           <div><CloseButton onClick={closeOverlay} /></div>
         </div>
         <ContainerText>
-          <div class={clsx(style.overlay_content, 'atrament-overlay')}>
+          <div class={overlayClasses}>
             {children}
           </div>
         </ContainerText>
--- src/components/screens/game/overlay/overlay.jsx
+++ src/components/screens/game/overlay/overlay.jsx
@@ -6,14 +6,32 @@ import { IconToolbarBack } from 'src/components/ui/icons';
 import ContainerText from 'src/components/ui/container-text';
 
 export default function OverlayPresenter({ children, title, closeOverlay }) {
+  const containerClasses = clsx(
+    style.overlay_container,
+    'atrament-overlay-container'
+  );
+  const headerClasses = clsx(
+    style.overlay_header,
+    style.overlay_header_bottom_line,
+    'atrament-overlay-header'
+  );
+  const titleClasses = clsx(
+    style.overlay_title,
+    'atrament-overlay-title'
+  );
+  const contentClasses = clsx(
+    style.overlay_content,
+    'atrament-overlay-content'
+  );
+
   return (
-    <div class={style.overlay_container}>
-      <div class={clsx(style.overlay_header, style.overlay_header_bottom_line)}>
+    <div class={containerClasses}>
+      <div class={headerClasses}>
         <button class={style.button_back} onClick={closeOverlay}><IconToolbarBack /></button>
-        {title && <div class={style.overlay_title}>{title}</div>}
+        {title && <div class={titleClasses}>{title}</div>}
       </div>
       <ContainerText>
-        <div class={clsx(style.overlay_content, 'atrament-overlay')}>
+        <div class={contentClasses}>
           {children}
         </div>
       </ContainerText>
