35a1d61266549aba9292c0ad7d68d76b1406ee7b
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-02-04 18:24:30 +0200

    load multiple externals from the same file; add utility example

--- src/atrament/externals.js
+++ src/atrament/externals.js
@@ -1,8 +1,12 @@
+import toArray from "src/utils/to-array";
 // import and register external functions
+
 export default function registerExternalFunctions(atrament) {
   const modules = import.meta.glob('../../resources/externals/*.js', { eager: true });
   Object.values(modules).map(({ default: def }) =>
-    atrament.ink.story().BindExternalFunction(def.name, def.external.bind(atrament), def.lookaheadSafe || false)
+    toArray(def).map((ext) =>
+      atrament.ink.story().BindExternalFunction(ext.name, ext.external.bind(atrament), ext.lookaheadSafe || false)
+    )
   );
 }
 
---
+++ src/utils/to-array.js
@@ -0,0 +1,3 @@
+export default function toArray(v) {
+  return Array.isArray(v) ? v : [v];
+}
