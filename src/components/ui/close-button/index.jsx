a3bfe980954eeadea7df1d08246da85992c92b70
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-03-06 09:11:24 +0200

    add translation for "Close"

--- src/components/ui/close-button/index.jsx
+++ src/components/ui/close-button/index.jsx
@@ -1,10 +1,14 @@
 import { h } from 'preact';
 import style from './index.module.css';
+import { useTranslator } from '@eo-locale/preact';
 
-const CloseButton = ({ onClick }) => (
-  <div class={style.close}>
-    <button onClick={onClick} title="Close">&#x2715;</button>
-  </div>
-);
+const CloseButton = ({ onClick }) => {
+  const translator = useTranslator();
+  return (
+    <div class={style.close}>
+      <button onClick={onClick} title={translator.translate('close')}>&#x2715;</button>
+    </div>
+  );
+};
 
 export default CloseButton;
