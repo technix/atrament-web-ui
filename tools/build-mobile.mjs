3fa0faa9ac40b9f20b50bfc9f4c412fcb92c004c
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-04-23 16:51:34 +0300

    Add iOS build (untested)

--- tools/build-mobile.mjs
+++ tools/build-mobile.mjs
@@ -31,12 +31,14 @@ Example Android configuration in atrament.config.json:
 // stop script in case of failures
 shell.config.fatal = true;
 
+const TARGET_PLATFORM = process.argv[2] || 'android';
+
 const DEFAULT_ICON = 'pwa-512x512.png';
 const DEFAULT_ORIENTATION = 'portrait';
 
 const APP_DIR = 'build/.tmp_standalone';
 const APP_WWW_DIR = `${APP_DIR}/resources`;
-const BUILD_DIR = 'build/.cache_android_build';
+const BUILD_DIR = TARGET_PLATFORM === 'android' ? 'build/.cache_android_build' : 'build/ios';
 const BUILD_WWW_DIR = `${BUILD_DIR}/www`;
 const OUTPUT_DIR = 'build/android';
 
@@ -49,36 +51,30 @@ const CORDOVA_BUILD_JSON = `${TOOLS_DIR}/build.json`;
 
 const DOCKER_BUILDER = 'frenzytechnix/atrament-android-builder';
 
+
 // read Atrament config
 const cfg = JSON.parse(fs.readFileSync('atrament.config.json', 'utf8'));
 
-const androidConfig = cfg.platform?.android;
+const mobileConfig = cfg.platform?.[TARGET_PLATFORM];
 
 const CORDOVA_CONFIG = {
-  id: androidConfig?.id,
-  name: androidConfig?.name,
-  version: androidConfig?.version,
-  description: androidConfig?.description,
-  author: androidConfig?.author,
-  email: androidConfig?.email,
-  website: androidConfig?.website,
-  icon: `www/${androidConfig?.icon || DEFAULT_ICON}`,
-  orientation: androidConfig?.orientation || DEFAULT_ORIENTATION
+  id: mobileConfig?.id,
+  name: mobileConfig?.name,
+  version: mobileConfig?.version,
+  description: mobileConfig?.description,
+  author: mobileConfig?.author,
+  email: mobileConfig?.email,
+  website: mobileConfig?.website,
+  icon: `www/${mobileConfig?.icon || DEFAULT_ICON}`,
+  orientation: mobileConfig?.orientation || DEFAULT_ORIENTATION
 };
 
-const missing_keys = Object.keys(CORDOVA_CONFIG).filter((k) => !CORDOVA_CONFIG[k]);
-if (missing_keys.length > 0) {
-  console.log("[!] ERROR: missing Android configuration options in atrament.config.json");
-  missing_keys.forEach(k => console.log(`- platform.android.${k}`));
-  process.exit(1);
-}
-
-const PACKAGETYPE = androidConfig?.debug ? 'debug' : 'release';
+const PACKAGETYPE = mobileConfig?.debug ? 'debug' : 'release';
 let CLEARBUILD = "";
 
 const docker = new Docker();
 
-function prepareAndroidBuild() {
+function prepareMobileBuild() {
   if (!fs.existsSync(BUILD_DIR)) {
     fs.mkdirSync(BUILD_DIR);
   } else {
@@ -91,9 +87,9 @@ function prepareAndroidBuild() {
   if (!fs.existsSync(BUILD_WWW_DIR)) {
     fs.mkdirSync(BUILD_WWW_DIR);
   }
-  console.log('>>> Copy app files to build dir');
+  console.log('>>> Move app files to build dir');
   shell.mv('-f', `${APP_WWW_DIR}/*`, `${BUILD_WWW_DIR}`);
-  console.log('>>> Configure Android project');
+  console.log('>>> Configuring Cordova project');
   const packageTemplate = fs.readFileSync(CORDOVA_PACKAGE_JSON, 'utf8');
   fs.writeFileSync(
     `${BUILD_DIR}/package.json`,
@@ -104,8 +100,10 @@ function prepareAndroidBuild() {
     `${BUILD_DIR}/config.xml`,
     Mustache.render(configTemplate, CORDOVA_CONFIG)
   );
-  shell.cp('-f', CORDOVA_BUILD_SH, BUILD_DIR);
-  shell.cp('-f', CORDOVA_BUILD_JSON, BUILD_DIR);
+  if (TARGET_PLATFORM === 'android') {
+    shell.cp('-f', CORDOVA_BUILD_SH, BUILD_DIR);
+    shell.cp('-f', CORDOVA_BUILD_JSON, BUILD_DIR);
+  }
 }
 
 function copyAndroidPackages() {
@@ -214,11 +212,39 @@ function followPullProgress(err, stream) {
     } else {
       console.log(' Done.');
       // prepare project folder
-      prepareAndroidBuild();
+      prepareMobileBuild();
       // run builder
       runAndroidBuild();
     }
   }
+}
+
+
+// ============================================================================================
+
+
+// check for missing configuration keys
+const missing_keys = Object.keys(CORDOVA_CONFIG).filter((k) => !CORDOVA_CONFIG[k]);
+if (missing_keys.length > 0) {
+  console.log("[!] ERROR: missing configuration options in atrament.config.json");
+  missing_keys.forEach(k => console.log(`- platform.${TARGET_PLATFORM}.${k}`));
+  process.exit(1);
+}
+
+if (TARGET_PLATFORM === 'ios') {
+  console.log('>>> Preparing build.');
+  // prepare Cordova project for iOS
+  prepareMobileBuild();
+  shell.pushd('-q', BUILD_DIR);
+  shell.exec('npm install cordova');
+  if (!fs.existsSync(`platforms/ios`)) {
+    shell.exec('npx cordova platform add ios');
+  }
+  shell.popd('-q');
+  shell.rm('-rf', APP_DIR);
+  console.log(`>>> Cordova project for iOS created in ${BUILD_DIR} folder.`);
+  console.log('>>> See Cordova documentation on building app for iOS: https://cordova.apache.org/docs/en/latest/guide/platforms/ios/index.html');
+  process.exit(0);
 }
 
 console.log('>>> Starting Android build.');
