5f18acd7378f70fd30e7ebe0baf29efd1012d243
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-06-09 18:51:45 +0300

    fix: grid grouped choice with background

--- src/components/screens/game/story/choice-button-group/index.jsx
+++ src/components/screens/game/story/choice-button-group/index.jsx
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 import clsx from 'clsx';
-import { useState, useCallback } from 'preact/hooks';
+import { useState, useCallback, useRef } from 'preact/hooks';
 import { useAtrament, useAtramentState } from 'src/atrament/hooks';
 import { useKeyboardHandler } from 'src/hooks';
 import { SINGLE_CHOICE_DELAY, MULTI_CHOICE_DELAY } from 'src/constants';
@@ -8,10 +8,13 @@ import ChoiceButton from '../choice-button';
 import ChoiceTimer from '../choice-timer';
 import style from './index.module.css';
 
+const noOp = () => {};
+
 const ChoiceButtonGroup = ({ key, currentScene, setReady }) => {
   const { makeChoice, continueStory } = useAtrament();
   const { metadata } = useAtramentState(['metadata']);
   const [ chosen, setChosen ] = useState(null);
+  const choiceGroupRef = useRef(null);
 
   // hide choices with #HIDDEN tag
   const availableChoices = currentScene.choices?.filter((i) => !i.tags?.HIDDEN);
@@ -20,40 +23,7 @@ const ChoiceButtonGroup = ({ key, currentScene, setReady }) => {
 
   const delay = numberOfChoices > 1 ? MULTI_CHOICE_DELAY : SINGLE_CHOICE_DELAY;
 
-  const selectChoice = useCallback((id) => {
-    setChosen(id);
-    // pass choice to Atrament
-    setTimeout(() => {
-      setChosen(null);
-      setReady(false);
-      makeChoice(id);
-      continueStory();
-    }, delay);
-  }, [ makeChoice, continueStory, setReady, delay ]);
-
-  const kbdChoiceHandler = useCallback((e) => {
-    if (chosen !== null) {
-      // only single keyboard choice can be made
-      return;
-    }
-    const kbdChoice = +e.key;
-    const targetElement = e.target.tagName.toLowerCase();
-    if (targetElement === 'input') {
-      // ignore keyboard event
-      return;
-    }
-    if (
-      numberOfChoices > 0 &&
-      kbdChoice > 0 &&
-      kbdChoice <= numberOfChoices &&
-      !availableChoices[kbdChoice-1].disabled
-    ) {
-      selectChoice(availableChoices[kbdChoice-1].id);
-    }
-  }, [ numberOfChoices, selectChoice, availableChoices, chosen ]);
-
-  useKeyboardHandler(kbdChoiceHandler);
-
+  // parse choices configuration
   // knot tag '#CHOICES' has more priority than global '#choices' tag
   const choiceConfig = currentScene.tags?.CHOICES || metadata.choices;
 
@@ -79,6 +49,45 @@ const ChoiceButtonGroup = ({ key, currentScene, setReady }) => {
     gridColumns: gridColumns
   };
 
+  const choiceGroupHandlerFn = choiceAppearance.grouped && choiceAppearance.grid
+    ? () => choiceGroupRef.current.style.setProperty("background-color", "var(--bg-color)")
+    : noOp;
+
+  const selectChoice = useCallback((id) => {
+    choiceGroupHandlerFn();
+    setChosen(id);
+    // pass choice to Atrament
+    setTimeout(() => {
+      setChosen(null);
+      setReady(false);
+      makeChoice(id);
+      continueStory();
+    }, delay);
+  }, [ makeChoice, continueStory, setReady, delay, choiceGroupHandlerFn ]);
+
+  const kbdChoiceHandler = useCallback((e) => {
+    if (chosen !== null) {
+      // only single keyboard choice can be made
+      return;
+    }
+    const kbdChoice = +e.key;
+    const targetElement = e.target.tagName.toLowerCase();
+    if (targetElement === 'input') {
+      // ignore keyboard event
+      return;
+    }
+    if (
+      numberOfChoices > 0 &&
+      kbdChoice > 0 &&
+      kbdChoice <= numberOfChoices &&
+      !availableChoices[kbdChoice-1].disabled
+    ) {
+      selectChoice(availableChoices[kbdChoice-1].id);
+    }
+  }, [ numberOfChoices, selectChoice, availableChoices, chosen ]);
+
+  useKeyboardHandler(kbdChoiceHandler);
+
   const choiceButtonGroupClass = clsx(
     choiceAppearance.row && style.choice_row,
     (choiceAppearance.row && !choiceAppearance.grouped) && style.choice_row_ungrouped,
@@ -98,7 +107,7 @@ const ChoiceButtonGroup = ({ key, currentScene, setReady }) => {
           selectChoice={selectChoice}
         />
       }
-      <div class={choiceButtonGroupClass} style={{ "--grid-columns": gridColumns }}>
+      <div ref={choiceGroupRef} class={choiceButtonGroupClass} style={{ "--grid-columns": gridColumns }}>
         {availableChoices.map((choice, index) => (
           <ChoiceButton
             key={`${key}-${index}`}
